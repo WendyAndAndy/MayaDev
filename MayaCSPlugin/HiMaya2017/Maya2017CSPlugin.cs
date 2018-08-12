706b1202207470e1875494fde119ff1d65d2f603
WendyAndAndy <184327932@qq.com>
2018-08-12 09:41:07 +0800

    cs plugin for Maya2017
    + HiMaya2017

---
+++ MayaCSPlugin/HiMaya2017/Maya2017CSPlugin.cs
@@ -0,0 +1,34 @@
+﻿using Autodesk.Maya.OpenMaya;
+
+[assembly: ExtensionPlugin(typeof(WendyAndAndy.Maya2017CSPlugin))]
+[assembly: MPxCommandClass(typeof(WendyAndAndy.HiMayaCmd), "csHi")]
+
+namespace WendyAndAndy
+{
+    public class Maya2017CSPlugin : IExtensionPlugin
+    {
+        public string GetMayaDotNetSdkBuildVersion()
+        {
+            string version = "201780";
+            return version;
+        }
+
+        public bool InitializePlugin()
+        {
+            return true;
+        }
+
+        public bool UninitializePlugin()
+        {
+            return true;
+        }
+    }
+
+    class HiMayaCmd : MPxCommand,IMPxCommand
+    {
+        public override void doIt(MArgList args)
+        {
+            MGlobal.displayInfo("你好，Maya2017！The message come from a C# plugin.");
+        }
+    }
+}
